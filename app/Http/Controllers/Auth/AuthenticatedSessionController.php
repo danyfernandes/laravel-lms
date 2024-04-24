f58ccaa5ad8f4def163562f60fb93f4b208fbd9f
danyfernandes <fernandesda4@gmail.com>
2024-04-24 21:38:07 +0100

    Fix bug when redirecting user after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,11 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if (auth()->user()->is_admin) {
+            return redirect()->intended(route('admin.dashboard', absolute: false));
+        }
+
+        return redirect()->intended(route('student.dashboard', absolute: false));
     }
 
     /**
