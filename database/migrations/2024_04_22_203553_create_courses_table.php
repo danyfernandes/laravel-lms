69fc1c5d9b0ce564c87bc926ecf38feac7328818
danyfernandes <fernandesda4@gmail.com>
2024-05-01 09:01:18 +0100

    Add excerpt column to course migration schema

--- database/migrations/2024_04_22_203553_create_courses_table.php
+++ database/migrations/2024_04_22_203553_create_courses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->string('excerpt');
             $table->string('image_path');
             $table->string('slug');
             $table->string('price');
